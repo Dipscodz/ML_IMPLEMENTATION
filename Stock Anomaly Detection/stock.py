7cf64a7f3ee1cd049cee3cf1c3790d901653fb02
DERIN DENNY MATHEW <derindenny65@gmail.com>
2026-02-25 11:49:07 +0530

    Refactor anomaly detection to use KMeans clustering. Updated logic for anomaly identification and adjusted plot title.

--- Stock Anomaly Detection/stock.py
+++ Stock Anomaly Detection/stock.py
@@ -1,26 +1,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn.mixture as GMM
+from sklearn.cluster import KMeans
+
 
 df = pd.read_csv("pca_data.csv")
-
 X = df[["PCA1", "PCA2"]]
 
-gmm = GMM.GaussianMixture(n_components=2, covariance_type="full", random_state=42)
-df["Cluster"] = gmm.fit_predict(X)
 
-log_likelihood = gmm.score_samples(X)
-threshold = np.percentile(log_likelihood, 5)
-df["Anomaly"] = (log_likelihood < threshold).astype(int)
+kmeans = KMeans(n_clusters=2, random_state=42)
+df["Cluster"] = kmeans.fit_predict(X)
+
+
+
+
+distances = kmeans.transform(X)
+
+
+min_distance = np.min(distances, axis=1)
+
+threshold = np.percentile(min_distance, 95)
+
+df["Anomaly"] = (min_distance > threshold).astype(int)
+
 
 print(df["Cluster"].value_counts())
 print(df["Anomaly"].value_counts())
 
+
 plt.scatter(df["PCA1"], df["PCA2"], c=df["Cluster"], cmap="viridis", label="Clusters")
-plt.scatter(df[df["Anomaly"] == 1]["PCA1"], df[df["Anomaly"] == 1]["PCA2"], c="red", label="Anomalies", edgecolor="k")
+plt.scatter(df[df["Anomaly"] == 1]["PCA1"],
+            df[df["Anomaly"] == 1]["PCA2"],
+            c="red", label="Anomalies", edgecolor="k")
+
 plt.xlabel("PCA1")
 plt.ylabel("PCA2")
-plt.title("GMM Clustering and Anomaly Detection")
+plt.title("KMeans Clustering and Anomaly Detection")
 plt.legend()
 plt.show()
